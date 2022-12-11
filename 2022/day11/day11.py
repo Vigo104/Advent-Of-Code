553dceb0766195124dc5be413cd8a259a2daabcc
Vigo104 <58277954+Vigo104@users.noreply.github.com>
2022-12-11 20:48:07 +0100

    day11

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -5,71 +5,77 @@ SCRIPT_DIR = Path(__file__).parent
 # INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
 INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
 
+class Monkey():
+	def __init__(self):
+		self.index = 0
+		self.items = []
+		self.op = ["", ""]
+		self.test = 0
+		self.dest = [0, 0]
+
+def parseInput(data):
+	monkeys = [Monkey() for _ in data]
+	for i, _ in enumerate(data):
+		monkeys[i].index = int(data[i][0][:-1][-1:])
+		s = data[i][1].split(":")
+		monkeys[i].items = [*map(int, s[1].split(","))]
+		s = data[i][2].split(" ")
+		s = s[-2:]
+		monkeys[i].op[0] = s[0]
+		monkeys[i].op[1] = s[1]
+		s = data[i][3].split(" ")
+		monkeys[i].test = int(s[-1:][0])
+		monkeys[i].dest[0] = int(data[i][4][-1:][0])
+		monkeys[i].dest[1] = int(data[i][5][-1:][0])
+	return monkeys
+
 def main():
 	with open(INPUT_FILE, mode="rt") as f:
 		data = [x.splitlines() for x in f.read().split("\n\n")]
-    
-	for i, monkey in enumerate(data):
-		for j, line in enumerate(monkey):
-			if j == 0:
-				data[i][j] = int(data[i][j][:-1][-1:])
-			elif j == 1:
-				s = data[i][j].split(":")
-				data[i][j] = [*map(int, s[1].split(","))]
-			elif j == 2:
-				s = data[i][j].split(" ")
-				s = s[-2:]
-				data[i][j] = [s[0], s[1]]
-			elif j == 3:
-				s = data[i][j].split(" ")
-				data[i][j] = int(s[-1:][0])
-			elif j == 4:
-				data[i][j] = int(data[i][j][-1:][0])
-			elif j == 5:
-				data[i][j] = int(data[i][j][-1:][0])
+	monkeys = parseInput(data)
 
-	dataCopy = copy.deepcopy(data)
+	monkeysCopy = copy.deepcopy(monkeys)
 	monkeyBusiness = [0 for _ in range(len(data))]
-	for monkey in data * 20:
-		for n in monkey[1]:
-			monkeyBusiness[monkey[0]] += 1
-			if monkey[2][1].isdigit():
-				if monkey[2][0] == "+":
-					n += int(monkey[2][1])
-				elif monkey[2][0] == "*":
-					n *= int(monkey[2][1])
+	for monkey in monkeys * 20:
+		for worryLevel in monkey.items:
+			monkeyBusiness[monkey.index] += 1
+			if monkey.op[1].isdigit():
+				if monkey.op[0] == "+":
+					worryLevel += int(monkey.op[1])
+				elif monkey.op[0] == "*":
+					worryLevel *= int(monkey.op[1])
 			else:
-				n *= n
-			n = n//3
-			if n % monkey[3] == 0:
-				data[monkey[4]][1].append(n)
+				worryLevel *= worryLevel
+			worryLevel = worryLevel//3
+			if worryLevel % monkey.test == 0:
+				monkeys[monkey.dest[0]].items.append(worryLevel)
 			else:
-				data[monkey[5]][1].append(n)
-		data[monkey[0]][1].clear()
+				monkeys[monkey.dest[1]].items.append(worryLevel)
+		monkey.items.clear()
 	result = sorted(set(monkeyBusiness), reverse=True)
 	print("Part 1:", result[0] * result[1])
 
-	data = copy.deepcopy(dataCopy)
+	monkeys = copy.deepcopy(monkeysCopy)
 	magicNumber = 1
-	for monkey in data:
-		magicNumber *= monkey[3]
+	for monkey in monkeys:
+		magicNumber *= monkey.test
 	monkeyBusiness = [0 for _ in range(len(data))]
-	for monkey in data * 10000:
-		for n in monkey[1]:
-			monkeyBusiness[monkey[0]] += 1
-			if monkey[2][1].isdigit():
-				if monkey[2][0] == "+":
-					n += int(monkey[2][1])
-				elif monkey[2][0] == "*":
-					n *= int(monkey[2][1])
+	for monkey in monkeys * 10000:
+		for worryLevel in monkey.items:
+			monkeyBusiness[monkey.index] += 1
+			if monkey.op[1].isdigit():
+				if monkey.op[0] == "+":
+					worryLevel += int(monkey.op[1])
+				elif monkey.op[0] == "*":
+					worryLevel *= int(monkey.op[1])
 			else:
-				n *= n
-			n %= magicNumber
-			if n % monkey[3] == 0:
-				data[monkey[4]][1].append(n)
+				worryLevel *= worryLevel
+			worryLevel %= magicNumber
+			if worryLevel % monkey.test == 0:
+				monkeys[monkey.dest[0]].items.append(worryLevel)
 			else:
-				data[monkey[5]][1].append(n)
-		data[monkey[0]][1].clear()
+				monkeys[monkey.dest[1]].items.append(worryLevel)
+		monkey.items.clear()
 	result = sorted(set(monkeyBusiness), reverse=True)
 	print("Part 2:", result[0] * result[1])
 
